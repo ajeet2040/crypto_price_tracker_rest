3ba02aa61571d29358a8ddfc944c53cc2dfef684
ajeet2040 <ajeet2040>
2022-10-02 00:23:34 +0530

    price in response changed to int

--- price_tracker/serializers.py
+++ price_tracker/serializers.py
@@ -1,12 +1,22 @@
 """Module containing all serializers"""
 from rest_framework import serializers
 from price_tracker.models import PriceTracker
+from django.db import models
 
 
 class PriceTrackerSerializer(serializers.ModelSerializer):
     """
     Serializer for Price Tracker
     """
+
     class Meta:
         model = PriceTracker
         fields = ['timestamp', 'price', 'coin']
+
+    def to_representation(self, instance):
+        """
+        Change data type of price from decimal to int
+        """
+        representation = super().to_representation(instance)
+        representation['price'] = round(instance.price)
+        return representation
